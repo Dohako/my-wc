70d0efe872dee908225c350b0953b565f7fc3e55
Denis Pankratev <d.pankratev@raicoon.com>
2024-02-13 20:45:02 +0100

    first commit

---
+++ py/my_wc.py
@@ -0,0 +1,20 @@
+import argparse
+
+# Create the argument parser
+parser = argparse.ArgumentParser(description='Count the number of bytes in a file')
+
+# Add the file path argument
+parser.add_argument('file', type=str, help='Path to the file')
+
+# Parse the arguments
+args = parser.parse_args()
+
+# Read the file and count the bytes
+try:
+    with open(args.file, 'rb') as file:
+        byte_count = len(file.read())
+        print("Number of bytes in the file:", byte_count)
+except FileNotFoundError:
+    print("File not found:", args.file)
+except IOError:
+    print("Error reading file:", args.file)
