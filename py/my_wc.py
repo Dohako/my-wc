7dc09a671d7cb5adf2fba629b138f74d2300a4b7
Denis Pankratev <d.pankratev@raicoon.com>
2024-02-13 23:01:21 +0100

    [py] add different way of collecting info about bytes

--- py/my_wc.py
+++ py/my_wc.py
@@ -1,4 +1,5 @@
 import argparse
+import os
 
 # Create the argument parser
 parser = argparse.ArgumentParser(description='Count the number of bytes in a file')
@@ -11,9 +12,12 @@ args = parser.parse_args()
 
 # Read the file and count the bytes
 try:
-    with open(args.file, 'rb') as file:
-        byte_count = len(file.read())
-        print("Number of bytes in the file:", byte_count)
+    # stat way
+    file_stats = os.stat(args.file)
+    # straightforward way
+    # with open(args.file, 'rb') as file:
+    #     byte_count = len(file.read())
+    #     print("Number of bytes in the file:", byte_count)
 except FileNotFoundError:
     print("File not found:", args.file)
 except IOError:
